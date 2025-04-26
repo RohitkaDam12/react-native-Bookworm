8b3e815195e40be5fc138def5f3206fb80e7ae1a
Rohit Kadam <rohitkadam25635@gmail.com>
2025-04-26 12:00:41 +0530

    cron.js added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import "dotenv/config";
 import authRoutes from './routes/authRoutes.js';
 import { connectDB } from './lib/db.js';
 import bookRoutes from './routes/bookRoutes.js';
+import job from './lib/cron.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+job.start(); // Start the cron job
 
 app.use(express.json());
 
